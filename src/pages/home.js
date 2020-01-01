ee110cec6441916f21e6493dbd4cbb30cc221a0b
Frank Mathy <frankmathy@gmail.com>
2020-01-01 23:39:41 +0100

    Finished Post Card details

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,39 +1,44 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
 
-import Grid from '@material-ui/core/Grid';
+import Grid from "@material-ui/core/Grid";
+
+import Scream from "../components/Scream";
 
 class home extends Component {
-    state = {
-        screams: null
-    }
+  state = {
+    screams: null
+  };
 
-    componentDidMount() {
-        axios.get('/screams')
-            .then(res => {
-                console.log(res.data);
-                this.setState({
-                    screams: res.data
-                });
-            })
-            .catch(err => console.log(err));
-    }
+  componentDidMount() {
+    axios
+      .get("/screams")
+      .then(res => {
+        console.log(res.data);
+        this.setState({
+          screams: res.data
+        });
+      })
+      .catch(err => console.log(err));
+  }
 
-    render() {
-        let recentScreamsMarkup = this.state.screams ? (
-        this.state.screams.map(scream => <p>{scream.body}</p>)
-        ) : <p>Loading...</p>
-        return (
-            <Grid container spacing={16}>
-                <Grid item sm={8} xs={12}>
-                    {recentScreamsMarkup}
-                </Grid>
-                <Grid item sm={4} xs={12}>
-                    <p>Content...</p>
-                </Grid>
-            </Grid>
-        )
-    }
+  render() {
+    let recentScreamsMarkup = this.state.screams ? (
+      this.state.screams.map(scream => <Scream scream={scream} />)
+    ) : (
+      <p>Loading...</p>
+    );
+    return (
+      <Grid container spacing={16}>
+        <Grid item sm={8} xs={12}>
+          {recentScreamsMarkup}
+        </Grid>
+        <Grid item sm={4} xs={12}>
+          <p>Content...</p>
+        </Grid>
+      </Grid>
+    );
+  }
 }
 
-export default home
+export default home;
