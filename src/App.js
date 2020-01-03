c5d80fe1493cbf2ce695c8ab3ad53272ee79643f
Frank Mathy <frankmathy@gmail.com>
2020-01-03 01:18:55 +0100

    Fixed styles

--- src/App.js
+++ src/App.js
@@ -27,8 +27,34 @@ const theme = createMuiTheme({
       contrastText: "#fff"
     }
   },
-  typography: {
-    useNextVariants: true
+  spread: {
+    typography: {
+      useNextVariants: true
+    },
+    form: {
+      textAlign: "center"
+    },
+    image: {
+      margin: "20px auto 20px auto"
+    },
+    pageTitle: {
+      margin: "10px auto 10px auto"
+    },
+    textField: {
+      margin: "10px auto 10px auto"
+    },
+    button: {
+      margin: "20px auto 20px auto",
+      position: "relative"
+    },
+    customError: {
+      color: "red",
+      fontSize: "0.8rem",
+      marginTop: 10
+    },
+    progress: {
+      position: "absolute"
+    }
   }
 });
 
--- src/pages/signup.js
+++ src/pages/signup.js
@@ -12,32 +12,9 @@ import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import CircularProgress from "@material-ui/core/CircularProgress";
 
-const styles = {
-  form: {
-    textAlign: "center"
-  },
-  image: {
-    margin: "20px auto 20px auto"
-  },
-  pageTitle: {
-    margin: "10px auto 10px auto"
-  },
-  textField: {
-    margin: "10px auto 10px auto"
-  },
-  button: {
-    margin: "20px auto 20px auto",
-    position: "relative"
-  },
-  customError: {
-    color: "red",
-    fontSize: "0.8rem",
-    marginTop: 10
-  },
-  progress: {
-    position: "absolute"
-  }
-}
+const styles = (theme) => ({
+  ...(theme.spread)
+})
 
 class signup extends Component {
   constructor() {
